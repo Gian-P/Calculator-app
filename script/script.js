c19650f67858efccf21d97c426ce0b3440ee46e4
Gian-P <pimentel.giancarlo1@gmail.com>
2022-03-19 23:30:28 -0400

    Completed the keyboard functionality

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@ let TopAreaValue = null;
 let BottomAreaValue = null;
 let CurrentOperator = null;
 let OperatorArray = ["+","-","x","/"];
+root = document.documentElement;
+root.className = "theme-1"
 
 /*-----------------References to the elements on screen-----------------*/
 
@@ -11,6 +13,12 @@ let DisplayBottomArea = document.querySelector(".DisplayBottomArea");
 let NumericButtons = document.querySelectorAll(".NumericButton");
 let PointButton = document.querySelector(".PointButton");
 let OperatorButtons = document.querySelectorAll(".OperatorButton");
+let DeleteButton = document.querySelector(".Delete");
+let ResetButton = document.querySelector(".Reset");
+let EqualButton = document.querySelector(".Equal");
+let theme_1 = document.querySelector("#theme_1");
+let theme_2 = document.querySelector("#theme_2");
+let theme_3 = document.querySelector("#theme_3");
 
 /*-------------------------AddEventListeners-----------------------*/
 NumericButtons.forEach(element => {
@@ -22,11 +30,17 @@ OperatorButtons.forEach(element => {
 });
 
 PointButton.addEventListener("click",PrintNumber);
+DeleteButton.addEventListener("click",Delete);
+ResetButton.addEventListener("click",Reset);
+EqualButton.addEventListener("click",ExecuteOperationByEqual);
+theme_1.addEventListener("click",ChangeTheme);
+theme_2.addEventListener("click",ChangeTheme);
+theme_3.addEventListener("click",ChangeTheme);
 
 document.addEventListener("keydown",CallCorrespondingFunction);
 
 
-/*----------------------Referencias a los botones----------------------*/
+/*----------------------Calculator functions----------------------*/
 
 // Calls the function corresponding to the bottom that was selected if the character maches any of them.
 function CallCorrespondingFunction(e){
@@ -35,6 +49,12 @@ function CallCorrespondingFunction(e){
   OperatorArray.forEach(element => {
     if(e.key === element) CallOperatorFunction(e);
   })
+
+  if(e.key === "Backspace") Delete();
+
+  if(e.key === "Escape") Reset();
+
+  if(e.key === "Enter") ExecuteOperationByEqual(e);
 }
 
 // Prints only numbers or the "." to the bottom area of the display.
@@ -87,8 +107,35 @@ function ChangeOperator(textContent){
   CurrentOperator = textContent;
 }
 
-function ExecuteOperationByEqual(textContent){
-  let result = OperationFunctions.textContent;
+function Delete(e){
+  //let textContent = '';
+  let length = DisplayBottomArea.textContent.length;
+  //(e.type === "click") ? textContent = e.target.textContent : textContent = e.key;
+
+  DisplayBottomArea.textContent = DisplayBottomArea.textContent.slice(0,length-1);
+  if(DisplayBottomArea.textContent.length === 0){
+    DisplayBottomArea.textContent = "0";
+    BottomAreaValue = null;
+  }
+  else BottomAreaValue = DisplayBottomArea.textContent;
+}
+
+function Reset(){
+  DisplayTopArea.textContent = "";
+  DisplayBottomArea.textContent = "0";
+  TopAreaValue = null;
+  BottomAreaValue = null;
+  CurrentOperator = null;
+}
+
+function ExecuteOperationByEqual(e){
+  let result = CalculateResult(CurrentOperator);
+  if(result === null) return;
+  DisplayTopArea.textContent = "";
+  DisplayBottomArea.textContent = result;
+  TopAreaValue = null;
+  CurrentOperator = null;
+  BottomAreaValue = DisplayBottomArea.textContent;
 }
 
 function CalculateResult(textContent){
@@ -99,17 +146,31 @@ function CalculateResult(textContent){
     'x': parseFloat(TopAreaValue) * parseFloat(BottomAreaValue),
     '/': parseFloat(TopAreaValue) / parseFloat(BottomAreaValue)
   }
-  /*
-  let OperationFunctions = {
-    '+': parseInt(TopAreaValue) + parseInt(BottomAreaValue),
-    '-': parseInt(TopAreaValue) - parseInt(BottomAreaValue),
-    'x': parseInt(TopAreaValue) * parseInt(BottomAreaValue),
-    '/': parseInt(TopAreaValue) / parseInt(BottomAreaValue)
-  }*/
 
   if(Object.is(NaN, OperationFunctions[textContent]) === false && OperationFunctions[textContent] !== Infinity) return (OperationFunctions[textContent]).toFixed(2);
   return null;
 }
 
-root = document.documentElement;
-root.className = "theme-1";
+/*----------------------Toogle functions----------------------*/
+
+function ChangeTheme(e){
+  let textContent = '';
+  (e.type === "click") ? textContent = e.target.textContent : textContent = e.key;
+
+  if(textContent === "1"){
+    document.getElementById("circle-theme").style.justifyContent = "flex-start";
+    console.log('hola');
+    //root.className = "theme-1"
+
+  }
+  else if(textContent === "2"){
+    document.getElementById("circle-theme").style.justifyContent = "center";
+    //root.className = "theme-2"
+   
+  }
+  else{
+    document.getElementById("circle-theme").style.justifyContent = "end";
+    //root.className = "theme-3"
+  }
+}
+
